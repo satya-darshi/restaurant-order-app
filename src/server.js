ceeb457c0d4c39ff51cef19fde9e13a1b4d075e1
Satyadarshi <satyadarshi1331@gmail.com>
2025-06-29 21:07:05 +0530

    added database details

--- src/server.js
+++ src/server.js
@@ -199,4 +199,5 @@ app.get('/payments/:order_id', (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
